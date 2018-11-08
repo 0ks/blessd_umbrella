23c4c4ce2c39a94bfcd0768c8b54744e16a36991
kelvinst <kelvin.stinghen@gmail.com>
2018-11-08 15:28:20 -0200

    New phoenix deps

--- apps/blessd_web/mix.exs
+++ apps/blessd_web/mix.exs
@@ -37,15 +37,15 @@ defmodule BlessdWeb.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:blessd, in_umbrella: true},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:blessd, in_umbrella: true},
-      {:timex, "~> 3.4"},
-      {:cowboy, "~> 1.0"}
+      {:phoenix_pubsub, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:timex, "~> 3.4"}
     ]
   end
 
