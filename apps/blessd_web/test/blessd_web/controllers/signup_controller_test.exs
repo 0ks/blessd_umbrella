4187c29e60ca6a8073bbc5a8fd86d372a64786d3
Kelvin Stinghen <kelvin.stinghen@me.com>
2018-11-17 13:03:06 -0200

    Logging in after sign up (#22)

---
+++ apps/blessd_web/test/blessd_web/controllers/signup_controller_test.exs
@@ -0,0 +1,36 @@
+defmodule BlessdWeb.SignupControllerTest do
+  use BlessdWeb.ConnCase
+
+  @create_attrs %{
+    "church" => %{name: "Signup Church", identifier: "signup_church"},
+    "user" => %{name: "Signup User", email: "signup_user@mail.com"},
+    "credential" => %{source: "password", token: "password"}
+  }
+  @invalid_attrs %{
+    "church" => %{name: nil, identifier: nil},
+    "user" => %{name: nil, email: nil},
+    "credential" => %{source: nil, token: nil}
+  }
+
+  describe "new signup" do
+    test "renders form", %{conn: conn} do
+      conn = get(conn, Routes.signup_path(conn, :new))
+
+      assert html_response(conn, 200) =~ "Sign up"
+    end
+  end
+
+  describe "create user, church and credentials" do
+    test "redirects to index when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.signup_path(conn, :create), registration: @create_attrs)
+
+      assert redirected_to(conn) == Routes.person_path(conn, :index, "signup_church")
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.signup_path(conn, :create), registration: @invalid_attrs)
+
+      assert html_response(conn, 200) =~ "Sign up"
+    end
+  end
+end
